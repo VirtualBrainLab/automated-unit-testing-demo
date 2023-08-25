db83ca4c0c297347a7386361731f674ff2202204
Kenneth Yang <kjy5@uw.edu>
2023-08-25 13:44:04 -0700

    Better tests, catches errors

--- tests/test_demo_class.py
+++ tests/test_demo_class.py
@@ -16,13 +16,24 @@ class TestDemoClass(TestCase):
         """Test return_data function"""
         self.assertEqual(self.demo.return_data(), '{name: Demo, data: hello}')
 
+        new_demo = DemoClass('')
+        self.assertEqual(new_demo.return_data(), '{name: , data: hello}')
+
+        weird_demo = DemoClass(7)
+        self.assertEqual(weird_demo.return_data(), '{name: 7, data: hello}')
+
     def test_vector_sum(self):
         """Test vector_sum function"""
         self.assertEqual(self.demo.vector_sum((2, 2, 2), (2, 2, 2)), (4, 4, 4))
+        self.assertEqual(self.demo.vector_sum((2, 2, 2), (0, 0, 0)), (2, 2, 2))
+        self.assertEqual(self.demo.vector_sum((1, 2, 3), (4, 5, 6)), (5, 7, 9))
 
     def test_scale_vector(self):
         """Test scale_vector function"""
         self.assertEqual(self.demo.scale_vector(2, (2, 2, 2)), (4, 4, 4))
+        self.assertEqual(self.demo.scale_vector(0, (2, 2, 2)), (0, 0, 0))
+        self.assertEqual(self.demo.scale_vector(1, (2, 2, 2)), (2, 2, 2))
+        self.assertEqual(self.demo.scale_vector(2, (1, 2, 3)), (2, 4, 6))
 
     def test_sum_and_callback(self):
         """Test sum_and_callback function"""
